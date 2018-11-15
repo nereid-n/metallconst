50219225a8623175a0624beb6bd7efea00fde33b
nereid <nas.kuleshova@gmail.com>
2018-11-15 17:04:19 +0300

    add popular

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,8 +1,16 @@
-"use strict";
+'use strict';
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Shared = function Shared() {
-    _classCallCheck(this, Shared);
+  _classCallCheck(this, Shared);
 };
+
+if ($('.popular__slider-top').length > 0) {
+  $('.popular__slider-top').slick({
+    slidesToShow: 5,
+    prevArrow: '<button class="popular__slider-top-prev btn-prev btn-slider"><img src="assets/images/icons/icon-arrow-left.png" alt=""></button>',
+    nextArrow: '<button class="popular__slider-top-next btn-next btn-slider"><img src="assets/images/icons/icon-arrow-right.png" alt=""></button>'
+  });
+}
 //# sourceMappingURL=script.js.map
---
+++ src/components/index/popular/popular.js
@@ -0,0 +1,7 @@
+if ($('.popular__slider-top').length > 0) {
+  $('.popular__slider-top').slick({
+    slidesToShow: 5,
+    prevArrow: '<button class="popular__slider-top-prev btn-prev btn-slider"><img src="assets/images/icons/icon-arrow-left.png" alt=""></button>',
+    nextArrow: '<button class="popular__slider-top-next btn-next btn-slider"><img src="assets/images/icons/icon-arrow-right.png" alt=""></button>'
+  });
+}
